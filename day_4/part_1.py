0102fa6321f17ff5ac02526f79bd0b53d12f9824
Matheus Pedroni <pnmatheus@protonmail.com>
2022-12-03 17:12:44 -0300

    day 4

---
+++ day_4/part_1.py
@@ -0,0 +1,27 @@
+input = open("input.txt").read().splitlines()
+
+chosen_numbers = [int(x) for x in input.pop(0).split(",")]
+input.pop(0)
+
+boards = []
+board = []
+for line in input:
+    if not line and board:
+        boards.append(board)
+        board = []
+        continue
+    board.append([int(x) for x in line.split(' ') if x])
+
+if board:
+    boards.append(board)
+
+chosen = set()
+for number in chosen_numbers:
+    chosen.add(number)
+    for j, board in enumerate(boards):
+        columns = list(zip(*board))
+        if any(set(row).issubset(chosen) for row in board) or any(set(col).issubset(chosen) for col in columns):
+            remaining = {number for row in board for number in row} - chosen
+            print(sum(remaining) * number)
+            print(f"winning board: {j}")
+            exit()
